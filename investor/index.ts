3abec54478ec086607a1c49575245f714374e669
Hammad Ali <55716079+iamhammadaali@users.noreply.github.com>
2023-06-11 02:07:41 +0500

    Add files via upload

---
+++ investor/index.ts
@@ -0,0 +1,61 @@
+import realsLogo from "./reals_logo.svg";
+import realsLogoLight from "./realsLogoLight.svg";
+import imgNotFound from "./img-not-found.jpeg";
+import heroSectionImg from "./headerSection.webp";
+import signInHome from './sign-in-home.svg';
+import kycHome from './kyc-home.svg';
+import topUpHome from './top-up-home.svg';
+import investHome from './invest-home.svg';
+import createHome from './create-home.svg';
+import aboutUs from "./about_us.svg";
+import call from "./call.svg";
+import internet from "./internet.svg";
+import mail from "./mail.svg";
+import targetIcon from "./campaign_target.svg";
+import campaignPeopleIcon from "./campaign_page_people.svg";
+import campaignExchangeIcon from "./campaign_exchange_icon.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import twitter from "./twitter.svg";
+import copyright from "./copyright.svg";
+import uploadBtn from "./uploadBtn.svg";
+import default_profie_avatar_link from "./default_profie_avatar_link";
+import getStartedWithGraphicsRightSide from './getStartedGraphicsRightSide.svg';
+import getStartedWithGraphicsLeftSide from './getStartedGraphicsLeftSide.svg';
+import cloudDownloadIcon from './cloud_download_icon.svg';
+import checkCircleIcon from './check_circle_icon.svg';
+import openLinkIcon from './open_link.svg';
+import secImageNotFount from './sec-image-not-found.svg';
+import dummyProfile from './dummy-profile-pic.png'
+
+export {
+  dummyProfile,
+  secImageNotFount,
+  realsLogo,
+  realsLogoLight,
+  imgNotFound,
+  heroSectionImg,
+  signInHome,
+  kycHome,
+  investHome,
+  createHome,
+  topUpHome,
+  aboutUs,
+  call,
+  internet,
+  mail,
+  facebook,
+  instagram,
+  twitter,
+  copyright,
+  uploadBtn,
+  default_profie_avatar_link,
+  getStartedWithGraphicsLeftSide,
+  getStartedWithGraphicsRightSide,
+  cloudDownloadIcon,
+  checkCircleIcon,
+  targetIcon,
+  campaignPeopleIcon,
+  campaignExchangeIcon,
+  openLinkIcon
+};
